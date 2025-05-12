d87b502320dd00bffe69ed08c8065cb1ec0ad8d1
amorykiet <95635041+amorykiet@users.noreply.github.com>
2025-05-12 08:26:08 +0700

    implement resource manager

---
+++ CasuEngine/engine/manager/ResourceManager.cpp
@@ -0,0 +1,33 @@
+#include "ResourceManager.h"
+
+void ResourceManager::LoadResources()
+{
+	// Load all resources if need
+	// This function is empty because the resources are loaded on demand
+	// when they are requested for the first time.
+	// You can implement a preloading mechanism here if needed.
+}
+
+void ResourceManager::UnloadResources()
+{
+	for (auto& pair : m_textures)
+	{
+		pair.second.reset();
+	}
+	m_textures.clear();
+	for (auto& pair : m_fonts)
+	{
+		pair.second.reset();
+	}
+	m_fonts.clear();
+	for (auto& pair : m_sounds)
+	{
+		pair.second.reset();
+	}
+	m_sounds.clear();
+	for (auto& pair : m_music)
+	{
+		pair.second.reset();
+	}
+	m_music.clear();
+}
---
+++ CasuEngine/engine/manager/ResourceManager.h
@@ -0,0 +1,171 @@
+#pragma once
+#include "../core/Singleton.h"
+#include "../core/Texture.h"
+#include "../core/Font.h"
+#include "../core/Sound.h"
+#include "../core/Music.h"
+#include <string>
+#include <unordered_map>
+#include <memory>
+
+class ResourceManager : public Singleton<ResourceManager>
+{
+public:
+    void LoadResources();
+    void UnloadResources();
+
+    template<typename T>
+    T* GetResource(const std::string& path);
+
+    template<typename T>
+    T* LoadResource(const std::string& path);
+
+    template<typename T>
+    void UnloadResource(const std::string& path);
+
+private:
+    std::unordered_map<std::string, std::shared_ptr<Texture>> m_textures;
+    std::unordered_map<std::string, std::shared_ptr<Font>> m_fonts;
+    std::unordered_map<std::string, std::shared_ptr<Sound>> m_sounds;
+    std::unordered_map<std::string, std::shared_ptr<Music>> m_music;
+};
+
+template<typename T>
+inline T* ResourceManager::GetResource(const std::string& path)
+{
+	if constexpr (std::is_same_v<T, TextureUnmanaged>)
+	{
+		auto it = m_textures.find(path);
+		if (it != m_textures.end())
+		{
+			return it->second.get();
+		}
+	}
+	else if constexpr (std::is_same_v<T, Font>)
+	{
+		auto it = m_fonts.find(path);
+		if (it != m_fonts.end())
+		{
+			return it->second.get();
+		}
+	}
+	else if constexpr (std::is_same_v<T, Sound>)
+	{
+		auto it = m_sounds.find(path);
+		if (it != m_sounds.end())
+		{
+			return it->second.get();
+		}
+	}
+	else if constexpr (std::is_same_v<T, Music>)
+	{
+		auto it = m_music.find(path);
+		if (it != m_music.end())
+		{
+			return it->second.get();
+		}
+	}
+	else
+	{
+		return LoadResource<T>(path);
+	}
+}
+
+template<typename T>
+inline T* ResourceManager::LoadResource(const std::string& path)
+{
+	if constexpr (std::is_same_v<T, TextureUnmanaged>)
+	{
+		auto it = m_textures.find(path);
+		if (it != m_textures.end())
+		{
+			return it->second.get();
+		}
+		else
+		{
+			auto texture = std::make_shared<TextureUnmanaged>(path);
+			m_textures[path] = texture;
+			return texture.get();
+		}
+	}
+	else if constexpr (std::is_same_v<T, Font>)
+	{
+		auto it = m_fonts.find(path);
+		if (it != m_fonts.end())
+		{
+			return it->second.get();
+		}
+		else
+		{
+			auto font = std::make_shared<Font>(path);
+			m_fonts[path] = font;
+			return font.get();
+		}
+	}
+	else if constexpr (std::is_same_v<T, Sound>)
+	{
+		auto it = m_sounds.find(path);
+		if (it != m_sounds.end())
+		{
+			return it->second.get();
+		}
+		else
+		{
+			auto sound = std::make_shared<Sound>(path);
+			m_sounds[path] = sound;
+			return sound.get();
+		}
+	}
+	else if constexpr (std::is_same_v<T, Music>)
+	{
+		auto it = m_music.find(path);
+		if (it != m_music.end())
+		{
+			return it->second.get();
+		}
+		else
+		{
+			auto music = std::make_shared<Music>(path);
+			m_music[path] = music;
+			return music.get();
+		}
+	}
+	return nullptr;
+}
+
+template<typename T>
+inline void ResourceManager::UnloadResource(const std::string& path)
+{
+	if constexpr (std::is_same_v<T, TextureUnmanaged>)
+	{
+		auto it = m_textures.find(path);
+		if (it != m_textures.end())
+		{
+			m_textures.erase(it);
+		}
+	}
+	else if constexpr (std::is_same_v<T, Font>)
+	{
+		auto it = m_fonts.find(path);
+		if (it != m_fonts.end())
+		{
+			m_fonts.erase(it);
+		}
+	}
+	else if constexpr (std::is_same_v<T, Sound>)
+	{
+		auto it = m_sounds.find(path);
+		if (it != m_sounds.end())
+		{
+			m_sounds.erase(it);
+		}
+	}
+	else if constexpr (std::is_same_v<T, Music>)
+	{
+		auto it = m_music.find(path);
+		if (it != m_music.end())
+		{
+			m_music.erase(it);
+		}
+	}
+}
